5d390689668cf14754521108588b0d78245686d7
Miikaar <miikakaa@gmail.com>
2015-10-06 11:08:47 +0300

    Initial commit

---
+++ src/mockesimerkki/Hinnoittelija.java
@@ -0,0 +1,21 @@
+package mockesimerkki;
+
+public class Hinnoittelija {
+
+    public float getAlennusProsentti(Asiakas asiakas, Tuote tuote) {
+        return 0.0f;
+    }
+
+    void aloita() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void setAlennusProsentti(Asiakas asiakas, float f) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void lopeta() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
